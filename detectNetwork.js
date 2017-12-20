d9209be3b9a407697e7bba2af2b6a4eba0181818
Richard Hai <haicsgl1@gmail.com>
2017-12-20 15:52:14 -0800

    Filled out detectNetwork() to check what network the CC is on

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,8 +11,38 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
+  var cardNumberArray = cardNumber.split('');
+  var twoDigitIdentifier = cardNumberArray[0] + cardNumberArray[1];
+  
+  if ((twoDigitIdentifier === '38' || twoDigitIdentifier === '39') && cardNumberArray.length === 14) {
+    return "Diner's Club";
+  } else if ((twoDigitIdentifier === '34' || twoDigitIdentifier === '37') && cardNumberArray.length === 15) {
+    return "American Express";
+  }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
+}
+
+
+var assertEqual = function(actual, expected, testName) {
+  if (actual === expected) {
+    return 'passed';
+  } else {
+    return 'failed ' + testName +', expected ' + expected + ' but got ' + actual;
+  }
+}
+
+// test cases:
+// var test1 = assertEqual(detectNetwork('343456789012345'), 'American Express', 'checks to see if credit card number is in American express or Diners club');
+// var test2 = assertEqual(detectNetwork('38345678901234'), "Diner's Club", 'checks to see if credit card number is in American express or Diners club');
+// var test3 = assertEqual(detectNetwork('39345678901234'), "Diner's Club", 'checks to see if credit card number is in American express or Diners club');
+// var test4 = assertEqual(detectNetwork('343456789012345'), 'American Express', 'checks to see if credit card number is in American express or Diners club');
+// var test5 = assertEqual(detectNetwork('373456789012345'), 'American Express', 'checks to see if credit card number is in American express or Diners club');
+
+// console.log(test1);
+// console.log(test2);
+// console.log(test3);
+// console.log(test4);
+// console.log(test5);
+
 
 
